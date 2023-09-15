501430886c7947f97eee4322d2bd860bca2c2066
C1-80781 <sarodedinesh70@gmail.com>
2023-09-15 07:49:51 +0530

    assignment 6

---
+++ assignment_6/A6_Q5.py
@@ -0,0 +1,10 @@
+#
+#
+# 5)Calculate the sum and average of the digits present in a string
+# Str1="Python83737@#8496"
+#
+str = input("Enter your string: ")
+list = list(str)
+
+for i in list:
+    if
