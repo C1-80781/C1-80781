0713a7532e3cf3b58792f2fc48d6dac907a12253
C1-80781 <sarodedinesh70@gmail.com>
2023-09-15 08:21:39 +0530

    assignment 6

---
+++ assignment_6/A6_Q7.py
@@ -0,0 +1,10 @@
+#
+# Write a NumPy program to create an array with the values 1, 7, 13, 105 and determine the size of the memory occupied by the array.
+#
+import numpy as np
+
+
+num = np.array([1, 7, 13, 105])
+size_num = num.nbytes
+
+print(f" The size of array {num} = {size_num}")
