30ff0ae5f82041e6f98da56cae141f13df6cff2c
C1-80781 <sarodedinesh70@gmail.com>
2023-09-15 08:48:27 +0530

    assignment 6

---
+++ assignment_6/A6_Q8.py
@@ -0,0 +1,13 @@
+#
+#
+# 8) Write a NumPy program to convert an data type into to a floating type.
+# a2 = np.array([10, 20, 30, 40, 50], dtype='float')
+# print(a2.dtype)
+#
+#
+import numpy as np
+
+a2 = np.array([10, 20, 30, 40, 50], dtype='float')
+
+print(f"Converted data type is into: {a2.dtype}")
+print(a2)
